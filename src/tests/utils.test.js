325c4959c9bdc82669a9719be2babe662cc1ddf6
JC Gomez <car_25_ixco@hotmail.com>
2020-09-22 12:44:17 -0500

    Integrating prettier

--- src/tests/utils.test.js
+++ src/tests/utils.test.js
@@ -1,35 +1,62 @@
-const test = require('tape');
-const sinon = require('sinon')
-const {getSteps, formatSteps, asyncIterator, iterateAsyncIterator} = require('../utils');
+const test = require("tape");
+const sinon = require("sinon");
+const {
+  getSteps,
+  formatSteps,
+  asyncIterator,
+  iterateAsyncIterator,
+} = require("../utils");
 
-test('getSteps', (t) => {
-    t.plan(2);
-    t.deepEqual(getSteps('test'), ['test'], 'should handle NON array values');
-    t.deepEqual(getSteps(['test']), ['test'], 'should handle array values');
+test("getSteps", (t) => {
+  t.plan(2);
+  t.deepEqual(getSteps("test"), ["test"], "should handle NON array values");
+  t.deepEqual(getSteps(["test"]), ["test"], "should handle array values");
 });
 
-test('formatSteps', (t) => {
-    t.plan(3);
+test("formatSteps", (t) => {
+  t.plan(3);
 
-    t.deepEqual(formatSteps([{}]), [{}], 'NON file steps')
-    t.deepEqual(formatSteps([{file: {entries:['variable']}}]), [{file: {entries:['variable']}, variables:[{type: 'text', name: 'variable', message: 'Value for "variable": '}]}], 'should handle string entries')
-    t.deepEqual(formatSteps([{file: {entries:[{type: 'text', name: 'test', message: 'test'}]}}]), [{file: {entries:[{type: 'text', name: 'test', message: 'test'}]}, variables:[{type: 'text', name: 'test', message: 'test'}]}], 'should handle object entries')
-})
+  t.deepEqual(formatSteps([{}]), [{}], "NON file steps");
+  t.deepEqual(
+    formatSteps([{ file: { entries: ["variable"] } }]),
+    [
+      {
+        file: { entries: ["variable"] },
+        variables: [
+          { type: "text", name: "variable", message: 'Value for "variable": ' },
+        ],
+      },
+    ],
+    "should handle string entries"
+  );
+  t.deepEqual(
+    formatSteps([
+      { file: { entries: [{ type: "text", name: "test", message: "test" }] } },
+    ]),
+    [
+      {
+        file: { entries: [{ type: "text", name: "test", message: "test" }] },
+        variables: [{ type: "text", name: "test", message: "test" }],
+      },
+    ],
+    "should handle object entries"
+  );
+});
 
-test('asyncIterator', (t) => {
-    t.plan(1);
+test("asyncIterator", (t) => {
+  t.plan(1);
 
-    const it = asyncIterator(1, () => Promise.resolve())
+  const it = asyncIterator(1, () => Promise.resolve());
 
-    t.equal(it[Symbol.asyncIterator](), it, 'Should return an iterator')
-})
+  t.equal(it[Symbol.asyncIterator](), it, "Should return an iterator");
+});
 
-test('iterateAsyncIterator', async (t) => {
-    t.plan(1)
-    const it = asyncIterator(1, () => Promise.resolve())
-    const spy = sinon.spy();
+test("iterateAsyncIterator", async (t) => {
+  t.plan(1);
+  const it = asyncIterator(1, () => Promise.resolve());
+  const spy = sinon.spy();
 
-    await iterateAsyncIterator(it, spy);
+  await iterateAsyncIterator(it, spy);
 
-    t.equal(spy.callCount, 1);
-})
+  t.equal(spy.callCount, 1);
+});
